1074f53832e70ba8fc9e99d2fdd00acfb57830f7
Peerapat Junkhaow <peerapat40595@gmail.com>
2022-11-03 16:15:37 +0700

    test_instantiate in contract.rs

--- src/contract.rs
+++ src/contract.rs
@@ -1,24 +1,33 @@
 #[cfg(not(feature = "library"))]
 use cosmwasm_std::entry_point;
 use cosmwasm_std::{Binary, Deps, DepsMut, Env, MessageInfo, Response, StdResult};
-// use cw2::set_contract_version;
+use cw2::set_contract_version;
 
 use crate::error::ContractError;
 use crate::msg::{ExecuteMsg, InstantiateMsg, QueryMsg};
+use crate::state::{Config, CONFIG};
 
-/*
 const CONTRACT_NAME: &str = "crates.io:cw-starter";
 const CONTRACT_VERSION: &str = env!("CARGO_PKG_VERSION");
- */
 
 #[cfg_attr(not(feature = "library"), entry_point)]
 pub fn instantiate(
-    _deps: DepsMut,
+    deps: DepsMut,
     _env: Env,
-    _info: MessageInfo,
-    _msg: InstantiateMsg,
+    info: MessageInfo,
+    msg: InstantiateMsg,
 ) -> Result<Response, ContractError> {
-    unimplemented!()
+    set_contract_version(deps.storage, CONTRACT_NAME, CONTRACT_VERSION)?;
+
+    let admin = msg.admin.unwrap_or(info.sender.to_string());
+    let validated_admin = deps.api.addr_validate(&admin)?;
+    let config = Config {
+        admin: validated_admin.clone(),
+    };
+    CONFIG.save(deps.storage, &config)?;
+    Ok(Response::new()
+        .add_attribute("action", "instantiate")
+        .add_attribute("admin", validated_admin.to_string()))
 }
 
 #[cfg_attr(not(feature = "library"), entry_point)]
@@ -37,4 +46,33 @@ pub fn query(_deps: Deps, _env: Env, _msg: QueryMsg) -> StdResult<Binary> {
 }
 
 #[cfg(test)]
-mod tests {}
+mod tests {
+    use crate::contract::instantiate; // the contract instantiate function
+    use crate::msg::InstantiateMsg;
+    use cosmwasm_std::attr; // helper to construct an attribute e.g. ("action", "instantiate")
+    use cosmwasm_std::testing::{mock_dependencies, mock_env, mock_info}; // mock functions to mock an environment, message info, dependencies // our instantate method
+
+    // Two fake addresses we will use to mock_info
+    pub const ADDR1: &str = "addr1";
+    pub const ADDR2: &str = "addr2";
+
+    #[test]
+    fn test_instantiate() {
+        // Mock the dependencies, must be mutable so we can pass it as a mutable, empty vector means our contract has no balance
+        let mut deps = mock_dependencies();
+        // Mock the contract environment, contains the block info, contract address, etc.
+        let env = mock_env();
+        // Mock the message info, ADDR1 will be the sender, the empty vec means we sent no funds.
+        let info = mock_info(ADDR1, &vec![]);
+
+        // Create a message where we (the sender) will be an admin
+        let msg = InstantiateMsg { admin: None };
+        // Call instantiate, unwrap to assert success
+        let res = instantiate(deps.as_mut(), env, info, msg).unwrap();
+
+        assert_eq!(
+            res.attributes,
+            vec![attr("action", "instantiate"), attr("admin", ADDR1)]
+        )
+    }
+}
