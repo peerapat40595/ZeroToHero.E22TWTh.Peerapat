50102dac130b30738b738434daf7e5e87d67d774
Peerapat Junkhaow <peerapat40595@gmail.com>
2022-11-04 10:34:03 +0700

    Change poll_id type from String to &str

--- src/contract.rs
+++ src/contract.rs
@@ -76,7 +76,7 @@ fn execute_create_poll(
         question: question.to_string(),
         options: opts,
     };
-    POLLS.save(deps.storage, poll_id, &poll)?;
+    POLLS.save(deps.storage, &poll_id, &poll)?;
 
     Ok(Response::new()
         .add_attribute("action", "create_poll")
@@ -90,14 +90,14 @@ fn execute_vote(
     poll_id: String,
     vote: String,
 ) -> Result<Response, ContractError> {
-    let poll = POLLS.may_load(deps.storage, poll_id.clone())?;
+    let poll = POLLS.may_load(deps.storage, &poll_id)?;
 
     match poll {
         Some(mut poll) => {
             // The poll exists
             BALLOTS.update(
                 deps.storage,
-                (info.sender, poll_id.clone()),
+                (info.sender, &poll_id),
                 |ballot| -> StdResult<Ballot> {
                     match ballot {
                         Some(ballot) => {
@@ -134,7 +134,7 @@ fn execute_vote(
             poll.options[position].1 += 1;
 
             // Save the update
-            POLLS.save(deps.storage, poll_id, &poll)?;
+            POLLS.save(deps.storage, &poll_id, &poll)?;
             Ok(Response::new()
                 .add_attribute("action", "vote")
                 .add_attribute("question", poll.question)
@@ -165,13 +165,13 @@ fn query_all_polls(deps: Deps, _env: Env) -> StdResult<Binary> {
 }
 
 fn query_poll(deps: Deps, _env: Env, poll_id: String) -> StdResult<Binary> {
-    let poll = POLLS.may_load(deps.storage, poll_id)?;
+    let poll = POLLS.may_load(deps.storage, &poll_id)?;
     to_binary(&PollResponse { poll })
 }
 
 fn query_vote(deps: Deps, _env: Env, address: String, poll_id: String) -> StdResult<Binary> {
     let validated_address = deps.api.addr_validate(&address).unwrap();
-    let vote = BALLOTS.may_load(deps.storage, (validated_address, poll_id))?;
+    let vote = BALLOTS.may_load(deps.storage, (validated_address, &poll_id))?;
 
     to_binary(&VoteResponse { vote })
 }
--- src/state.rs
+++ src/state.rs
@@ -22,5 +22,5 @@ pub struct Ballot {
 }
 
 pub const CONFIG: Item<Config> = Item::new("config");
-pub const POLLS: Map<String, Poll> = Map::new("polls");
-pub const BALLOTS: Map<(Addr, String), Ballot> = Map::new("ballots");
+pub const POLLS: Map<&str, Poll> = Map::new("polls");
+pub const BALLOTS: Map<(Addr, &str), Ballot> = Map::new("ballots");
